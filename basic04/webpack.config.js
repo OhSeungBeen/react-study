a5a8c5f71402613f53819e4d62c521b256d11d14
OhSeungBeen <osb1992@naver.com>
2021-08-02 12:21:59 +0900

    Add: basic04 setting

---
+++ basic04/webpack.config.js
@@ -0,0 +1,75 @@
+const path = require('path');
+const RefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+
+module.exports = {
+  name: 'basic04',
+  mode: 'development',
+  devtool: 'eval',
+  resolve: {
+    extensions: ['.js', '.jsx'],
+  },
+
+  entry: {
+    app: ['./client'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        loader: 'babel-loader',
+        options: {
+          presets: [
+            [
+              '@babel/preset-env',
+              {
+                targets: {
+                  browsers: ['> 1% in KR'],
+                },
+                debug: true,
+              },
+            ],
+            '@babel/preset-react',
+          ],
+          plugins: ['@babel/plugin-proposal-class-properties', 'react-refresh/babel'],
+        },
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader',
+          {
+            loader: 'css-loader',
+            options: {
+              modules: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.svg$/,
+        loader: 'file-loader',
+      },
+      {
+        test: /\.s[ac]ss$/i,
+        use: [
+          // Creates `style` nodes from JS strings
+          'style-loader',
+          // Translates CSS into CommonJS
+          'css-loader',
+          // Compiles Sass to CSS
+          'sass-loader',
+        ],
+      },
+    ],
+  },
+  plugins: [new RefreshWebpackPlugin()],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'app.js',
+    publicPath: '/dist/',
+  },
+  devServer: {
+    publicPath: '/dist/',
+    hot: true,
+  },
+};
